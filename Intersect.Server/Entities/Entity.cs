79a533b906267579d4f9b7b1c9b7530cfb08f6b4
WeylonSantana <weylonsantanaoff@gmail.com>
2023-02-22 21:41:01 -0300

    Fix not casting spells

--- Intersect.Server/Entities/Entity.cs
+++ Intersect.Server/Entities/Entity.cs
@@ -949,7 +949,7 @@ namespace Intersect.Server.Entities
         public virtual void Move(Direction moveDir, Player forPlayer, bool doNotUpdate = false,
             bool correction = false)
         {
-            if (Timing.Global.Milliseconds < MoveTimer || (!Options.Combat.MovementCancelsCast && IsCasting))
+            if (Timing.Global.Milliseconds < MoveTimer || (!Options.Combat.MovementCancelsCast && IsCasting && !IgnoreOnMoveSpell()))
             {
                 return;
             }
@@ -961,7 +961,7 @@ namespace Intersect.Server.Entities
 
             lock (EntityLock)
             {
-                if (this is Player && IsCasting && Options.Combat.MovementCancelsCast)
+                if (this is Player && IsCasting && Options.Combat.MovementCancelsCast && !IgnoreOnMoveSpell())
                 {
                     CastTime = 0;
                     CastTarget = null;
